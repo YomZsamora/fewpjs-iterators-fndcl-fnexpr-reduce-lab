c6dbd0e152787cc64c2b8473deb9a55415217412
yomZsamora <samora.y@adzumi.co.ke>
2022-01-21 08:01:43 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Sum of all the assembled batteries
-let totalBatteries = batteryBatches.reduce( (batteryAmount, assembledBatteries) => {
-   return batteryAmount + assembledBatteries
-})
+let totalBatteries = batteryBatches.reduce( (batteryAmount, assembledBatteries) => { return batteryAmount + assembledBatteries })
 
 
